0d55910f6545d67e38b30302f609c217f8c6208c
Daarns <nandana219@gmail.com>
2026-01-09 15:20:47 +0700

    Add turbopack config compatibility

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type {NextConfig} from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
 
+  turbopack: {
+  },
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glb|gltf)$/,
